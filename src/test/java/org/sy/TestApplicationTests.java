9fe78412f5fba51ecea3e679034d03eae1e0d92d
郭俊旺 <1994371810@qq.com>
2020-04-05 16:29:40 +0800

    init

---
+++ src/test/java/org/sy/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
